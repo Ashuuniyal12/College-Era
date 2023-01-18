0a4f255f89d8bb4bb5a99a8aaa5228022db0f00e
Ashutosh Uniyal <ashutoshuniyal012@gmail.com>
2023-01-18 09:39:12 +0530

    deployment change

--- server/index.js
+++ server/index.js
@@ -25,10 +25,8 @@ dotenv.config();
 mongoose
     .connect(process.env.MONGO_DB,
         { useNewUrlParser: true, useUnifiedTopology: true }
-    ).then(() => app.listen(process.env.PORT|| 5000, () => console.log(`Server is running on ${process.env.PORT|| 5000}`))).
+    ).then(() => app.listen(process.env.PORT || 5000, () => console.log(`Server is running on ${process.env.PORT || 5000}`))).
     catch((error) => console.log(error.message));
-
-
 
 //usage of routes
 app.use('/auth',AuthRoute);  
