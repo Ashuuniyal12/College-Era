3c24e47b3da801dd0896c2437f0912d5feb07a5b
Ashutosh Uniyal <ashutoshuniyal012@gmail.com>
2023-01-18 12:34:50 +0530

    deployment change

--- client/src/API/messageRequest.js
+++ client/src/API/messageRequest.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+// const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'https://collegeera-server.onrender.com' });
 
 export const getMessages = (id) => API.get(`/message/${id}`);
 export const addMessage = (message) => API.post("/message/", message);
