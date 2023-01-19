b16af36f187446aaa32dde0bfb775752fcc657de
Ashutosh Uniyal <ashutoshuniyal012@gmail.com>
2023-01-19 12:56:39 +0530

    first deployment front end

--- client/src/API/UserRequest.js
+++ client/src/API/UserRequest.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
-// const API = axios.create({ baseURL: 'https://collegeera-server.onrender.com' });
+ // const API = axios.create({ baseURL: 'http://localhost:5000' });
+ const API = axios.create({ baseURL: 'https://collegeera-server-v9vm.onrender.com' });
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem('profile')) {
--- client/src/API/messageRequest.js
+++ client/src/API/messageRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
-// const API = axios.create({ baseURL: 'https://collegeera-server.onrender.com' });
+ // const API = axios.create({ baseURL: 'http://localhost:5000' });
+ const API = axios.create({ baseURL: 'https://collegeera-server-v9vm.onrender.com' });
 
 export const getMessages = (id) => API.get(`/message/${id}`);
 export const addMessage = (message) => API.post("/message/", message);
